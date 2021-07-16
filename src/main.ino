644fb9a49acd9fd1cd944078dbb969a8db4a6fdb
Luca Schuster <luca@vb-schuster.de>
2021-07-16 16:22:26 +0200

    Changed step values

--- src/main.ino
+++ src/main.ino
@@ -10,12 +10,12 @@
 #define buffer_motor_1_button Ftduino::I8
 
 Engine station_motor_1(Ftduino::M1, Ftduino::C1, TriggerType::lightsensor,
-                       light_station_begin, 10000, station_motor_1_button, 100);
-Engine station_motor_2(Ftduino::M2, Ftduino::C2, TriggerType::steps,
+                       light_station_begin, 10000, station_motor_1_button, 1400);
+/*Engine station_motor_2(Ftduino::M2, Ftduino::C2, TriggerType::steps,
                        Ftduino::C1, 100, station_motor_2_button, 100);
 Engine elevator_motor_1(Ftduino::M3, Ftduino::C3, TriggerType::lightsensor,
                         light_station_end, 10000,
-                        elevator_motor_1_button_bottom, 100);
+                        elevator_motor_1_button_bottom, 100);*/
 // TODO: Implement buffer engine
 // Engine buffer_motor_1(Ftduino::M4, Ftduino::C4);
 
@@ -36,7 +36,7 @@ Engine elevator_motor_1(Ftduino::M3, Ftduino::C3, TriggerType::lightsensor,
  * M6:
  * M7:
  * M8:
- * C1:
+ * C1: Counter Motor 1
  * C2:
  * C3:
  * C4:
@@ -46,15 +46,17 @@ void setup() {
   ftduino.init();
   Serial.begin(9600);
   ftduino.input_set_mode(light_station_begin, Ftduino::RESISTANCE);
-  ftduino.input_set_mode(light_station_end, Ftduino::RESISTANCE);
+  ftduino.input_set_mode(station_motor_1_button, Ftduino::SWITCH);
+  ftduino.counter_set_mode(Ftduino::C1, Ftduino::C_EDGE_ANY);
   // TODO: Set pin modes for buttons
   station_motor_1.reset();
-  station_motor_2.reset();
-  elevator_motor_1.reset();
+/*  station_motor_2.reset();
+  elevator_motor_1.reset();*/
 }
 
 void loop() {
   station_motor_1.cycle();
-  station_motor_2.cycle();
-  elevator_motor_1.cycle();
+  Serial.println(station_motor_1.get_steps());
+  /*station_motor_2.cycle();
+  elevator_motor_1.cycle();*/
 }
