8ad6536bf653c24b187b1ae2f4f958f18fa73e0c
Yannis Storrer <yannis.storrer@web.de>
2022-05-20 15:29:47 +0200

    added slave controller
    Implemented a second slave controller with a simple functionality to
    implement buffering the cart.

--- slave_controller/src/main.cpp
+++ slave_controller/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Ftduino.h>
 
-void loop(){
-    
-}
+#define buffer_motor Ftduino::M4
+#define trigger_button Ftduino::I1
+#define finish_button Ftduino::I2
+#define start_button Ftduino::I3
+
+#define speed 20
+
+bool direction = true;
 
 void setup(){
-
+    ftduino.init();
+    ftduino.input_set_mode(finish_button, Ftduino::SWITCH);
+    ftduino.input_set_mode(trigger_button, Ftduino::SWITCH);
+    ftduino.input_set_mode(start_button, Ftduino::SWITCH);
+    ftduino.motor_set(buffer_motor, direction+1, speed);
+    delay(100);
+    while (ftduino.input_get(finish_button)) {}
+    ftduino.motor_set(buffer_motor, Ftduino::OFF, speed);
+    direction = !direction;
+}
+void loop(){
+    if(!ftduino.input_get(trigger_button)){
+        ftduino.motor_set(buffer_motor, direction+1, speed);
+        while((ftduino.input_get(finish_button) && direction) || (ftduino.input_get(start_button) && !direction)){}
+        ftduino.motor_set(buffer_motor, Ftduino::OFF, speed);
+        direction = !direction;
+}
 }
